e20f9a55eb3016ab6f8067f34a4b848a7be5b7bb
lanzoone30 <lanzoone.adrian30@gmail.com>
2026-09-25 15:26:43 -0300

    se integran el tema y la retroalimentacion de envio

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,10 +5,13 @@ import { initAlerts } from './components/alerts.js';
 import { initTurnoEstado } from './components/turno-estado.js';
 import { initTurnoForm } from './components/turno-form.js';
 import { initConfirmForms } from './shared/confirm-forms.js';
-
+import { initSubmitFeedback } from './shared/submit-feedback.js';
+import { initTheme } from './shared/theme.js';
+initTheme();
 initSidebar();
 initNav();
 initAlerts();
 initTurnoEstado();
 initTurnoForm();
 initConfirmForms();
+initSubmitFeedback();
